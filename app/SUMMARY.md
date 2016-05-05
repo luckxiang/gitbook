3eefe23e15ae4dc2e551de7d9376007238eeed8e
zhangxiang <1049242886@qq.com>
2016-05-05 20:29:47 +0800

    add

--- app/SUMMARY.md
+++ app/SUMMARY.md
@@ -2,5 +2,6 @@
 
 * [Introduction](README.md)
 * [Homebrew](homebrew.md)
+* [apt-get](apt-get.md)
 * [mit-scheme](mit-scheme.md)
 
--- book_push_to_github.py
+++ book_push_to_github.py
@@ -2,9 +2,21 @@
 # coding=utf-8
 
 import os
+import sys
 
-os.system('cp -rf /Users/xiang/GitBook/Library/Import/ ./ ')
-os.system('find . -name  ".git"  -mindepth 2|xargs rm -rf ')
-os.system('git add . ')
-os.system('git commit -m "add" ')
+os.system('cp -rf /Users/xiang/GitBook/Library/Import/ ./')
+os.system('find . -name  ".git"  -mindepth 2|xargs rm -rf')
+os.system('git add .')
+os.system('git commit -m "add"')
 os.system('git push origin master')
+
+if len(sys.argv) < 2:
+    paths = os.popen('ls -F |grep "/"').readlines()
+else:
+    paths = sys.argv
+    paths.pop(0)
+
+for path in paths:
+    path = path.strip('\n')
+    sys = 'cd %s;gitbook build;mv _book  %s;cp -rf %s /Users/xiang/Workspace/github/luckxiang.github.io/gbook/%s;rm -rf %s'%(path,path,path,path,path)
+    os.system(sys)
